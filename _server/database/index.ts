93dad4edd87f46fcab89b7666f517cb96766052b
Hokori23 <hokori23@qq.com>
2020-08-15 23:10:02 +0800

    Feat: 数据库模块

---
+++ _server/database/index.ts
@@ -0,0 +1,17 @@
+const { dataBaseConfig } = require('../../geekblog.config');
+const MYSQL2 = require('mysql2');
+
+const DB = async () => {
+  try {
+    const connection = await MYSQL2.createConnection(dataBaseConfig);
+    await connection.connect((err: Error) => {
+      if (err) {
+        Promise.reject(err);
+      }
+    });
+    return connection;
+  } catch (e) {
+    console.log('连接数据库失败');
+  }
+};
+module.exports = DB;
