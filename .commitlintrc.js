ee51f2d5ce39734415a6ebac3c281abe9f700ab8
Hokori23 <hokori23@qq.com>
2020-09-15 13:56:58 +0800

    chore: commitlint配置大小写

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -5,15 +5,15 @@ module.exports = {
       2,
       'always',
       [
-        'Feat', //新功能（feature）
-        'Fix', //修补bug
-        'Docs', //文档（documentation）
-        'Style', // 格式（不影响代码运行的变动）
-        'Refactor', //重构（即不是新增功能，也不是修改bug的代码变动）
-        'Test', //增加测试
-        'Chore', //构建过程或辅助工具的变动
-        'Revert', //回滚
-        'Temp' //临时保存
+        'feat', //新功能（feature）
+        'fix', //修补bug
+        'docs', //文档（documentation）
+        'style', // 格式（不影响代码运行的变动）
+        'refactor', //重构（即不是新增功能，也不是修改bug的代码变动）
+        'test', //增加测试
+        'chore', //构建过程或辅助工具的变动
+        'revert', //回滚
+        'temp' //临时保存
       ]
     ],
     'type-case': [0],
