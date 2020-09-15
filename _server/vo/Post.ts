a8396ef8b6f880bdc4cd37231d43534becff485d
Hokori23 <hokori23@qq.com>
2020-09-15 14:12:32 +0800

    feat: VO层 -- Post.ts && PostTags.ts

--- _server/vo/Post.ts
+++ _server/vo/Post.ts
@@ -1,34 +1,36 @@
+import { isUndef } from '@public';
+
 interface Post {
   id: number;
-  title: string;
   content: string;
-  cover_url: string;
   created_at: string;
-  last_modified_at: string;
   view_count: number;
   comment_count: number;
   is_hidden: boolean;
   is_locked: boolean;
   is_sticky: boolean;
-  slug: string;
   type: boolean;
+  title?: string;
+  cover_url?: string;
+  last_modified_at?: string;
+  tag?: string;
 }
 
-class Post implements Post{
+class Post implements Post {
   constructor(
     id: number,
-    title: string,
     content: string,
-    cover_url: string,
     created_at: string,
-    last_modified_at: string,
     view_count: number,
     comment_count: number,
     is_hidden: boolean,
     is_locked: boolean,
     is_sticky: boolean,
-    slug: string,
-    type: boolean
+    type: boolean,
+    title?: string,
+    cover_url?: string,
+    last_modified_at?: string,
+    tag?: string
   ) {
     this.id = id;
     this.title = title;
@@ -41,8 +43,91 @@ class Post implements Post{
     this.is_hidden = is_hidden;
     this.is_locked = is_locked;
     this.is_sticky = is_sticky;
-    this.slug = slug;
+    this.tag = tag;
     this.type = type;
+  }
+  toArray(): Array<any> {
+    const {
+      id,
+      content,
+      created_at,
+      view_count,
+      comment_count,
+      is_hidden,
+      is_locked,
+      is_sticky,
+      type,
+      title,
+      cover_url,
+      last_modified_at,
+      tag
+    } = this;
+    return [
+      id,
+      title,
+      content,
+      cover_url,
+      created_at,
+      last_modified_at,
+      view_count,
+      comment_count,
+      is_hidden,
+      is_locked,
+      is_sticky,
+      tag,
+      type
+    ];
+  }
+  // 检查参数完整性
+  checkIntegrity(params?: Array<string>): boolean {
+    if (params) {
+      for (let i = 0; i < params.length; i++) {
+        if (isUndef(this[params[i]])) {
+          return false;
+        }
+      }
+      return true;
+    } else {
+      const arr = this.toArray();
+      for (let i = 0; i < arr.length; i++) {
+        if (isUndef(arr[i])) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+  static clone(obj: Post): Post {
+    const {
+      id,
+      content,
+      created_at,
+      view_count,
+      comment_count,
+      is_hidden,
+      is_locked,
+      is_sticky,
+      type,
+      title,
+      cover_url,
+      last_modified_at,
+      tag
+    } = obj;
+    return new Post(
+      id,
+      content,
+      created_at,
+      view_count,
+      comment_count,
+      is_hidden,
+      is_locked,
+      is_sticky,
+      type,
+      title,
+      cover_url,
+      last_modified_at,
+      tag
+    );
   }
 }
 
