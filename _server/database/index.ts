7812a1ed2c04abd2e56bec80ad4fe0e83df0dc05
Hokori23 <hokori23@qq.com>
2020-09-29 23:04:57 +0800

    fix: 去除数据库连接检测, 同步设置为alter

--- _server/database/index.ts
+++ _server/database/index.ts
@@ -1,14 +1,16 @@
 const { dataBaseConfig } = require('../../geekblog.config');
 import { Sequelize } from 'sequelize';
 
-const { database, user, password, options} = dataBaseConfig;
+const { database, user, password, options } = dataBaseConfig;
 
 const DB = new Sequelize(database, user, password, options);
 
 (async () => {
   try {
-    await DB.authenticate();
-    console.log('Connection has been established successfully.');
+    // await DB.authenticate();
+    // console.log('Connection has been established successfully.');
+    await DB.sync({ alter: true });
+    console.log('All models were synchronized successfully.');
   } catch (error) {
     throw new Error(`Unable to connect to the database: ${error}`);
   }
