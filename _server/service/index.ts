85d3885b3f980bc753fb738c4b76190978b60a4b
Hokori23 <hokori23@qq.com>
2020-10-01 01:17:23 +0800

    feat: 导出PostCommentService

--- _server/service/index.ts
+++ _server/service/index.ts
@@ -1,10 +1,12 @@
 import UserService from './UserService';
 import PostService from './PostService';
 import PostTagService from './PostTagService';
+import PostCommentService from './PostCommentService';
 
-export { UserService, PostService, PostTagService };
+export { UserService, PostService, PostTagService, PostCommentService };
 export default {
   User: UserService,
   Post: PostService,
-  PostTag: PostTagService
+  PostTag: PostTagService,
+  PostComment: PostCommentService
 };
