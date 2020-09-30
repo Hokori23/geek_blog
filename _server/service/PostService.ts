06a2485d8bda76770786daccbed50411a50bcfda
Hokori23 <hokori23@qq.com>
2020-10-01 01:16:09 +0800

    feat: 添加事务 - 判断帖子标签是否存在并批量添加 && 添加withComments选项

--- _server/service/PostService.ts
+++ _server/service/PostService.ts
@@ -1,26 +1,51 @@
-import { PostAction } from '@action';
-import { Post } from '@vo';
+import { PostAction, PostTagAction } from '@action';
+import { Post, PostTag } from '@vo';
 import { Restful, isUndef } from '@public';
+import DB from '@database';
 
 /**
  * 发布帖子
  * @param { Post } post
+ * @param { string } post_tag_name
  * @param { number } userPower
  * @param { string } userAccount
  */
 const Create = async (
   post: Post,
+  post_tag_names: Array<string>,
   userPower: number,
   userAccount: string
 ): Promise<Restful> => {
+  const t = await DB.transaction();
   try {
     if (isUndef(userPower) || userPower > 1) {
       return new Restful(1, `创建失败, 权限不足`);
     }
     post.user_account = userAccount;
-    post = await PostAction.Create(post);
+
+    let promises: Array<Promise<any>> = [];
+
+    post_tag_names.forEach((tag_name) => {
+      promises.push(PostTagAction.Retrieve__ByTagName(tag_name));
+    });
+    promises.push(PostAction.Create(post, t));
+    const promiseValues = await Promise.all(promises);
+    post = promiseValues.pop();
+    const postTags = promiseValues;
+
+    promises = [];
+    postTags.forEach((postTag, idx) => {
+      if (!postTag) {
+        throw new Error(`${post_tag_names[idx]}标签不存在`);
+      }
+      promises.push(post.addPostTag(postTag, { transaction: t }));
+    });
+    await Promise.all(promises);
+    t.commit();
+    post = (await PostAction.Retrieve__ByID(<number>post.id)) as Post;
     return new Restful(0, '创建成功', post.toJSON());
   } catch (e) {
+    await t.rollback();
     return new Restful(99, `创建失败, ${e.message}`);
   }
 };
@@ -28,14 +53,18 @@ const Create = async (
 /**
  * 通过ID查找帖子
  * @param { number } id
+ * @param { boolean } withComments
  */
-const Retrieve__ByID = async (id: number): Promise<Restful> => {
+const Retrieve__ByID = async (
+  id: number,
+  withComments: boolean = true
+): Promise<Restful> => {
   try {
-    const existedPost = await PostAction.Retrieve__ByID(id);
+    const existedPost = await PostAction.Retrieve__ByID(id, withComments);
     if (!existedPost) {
       return new Restful(1, '帖子不存在');
     }
-    return new Restful(0, '查询成功', existedPost);
+    return new Restful(0, '查询成功', existedPost.toJSON());
   } catch (e) {
     return new Restful(99, `查询失败, ${e.message}`);
   }
@@ -45,17 +74,20 @@ const Retrieve__ByID = async (id: number): Promise<Restful> => {
  * 分页查询帖子
  * @param { number } page
  * @param { number } capacity
+ * @param { boolean } withComments
  * @param { boolean } isASC
  */
 const Retrieve__Page = async (
   page: number,
   capacity: number,
+  withComments: boolean = true,
   isASC: boolean = false
 ): Promise<Restful> => {
   try {
     const existedPosts = await PostAction.Retrieve__Page(
       (page - 1) * capacity,
       Number(capacity),
+      withComments,
       isASC
     );
     return new Restful(
@@ -75,12 +107,14 @@ const Retrieve__Page = async (
  * @param { number } page
  * @param { number } capacity
  * @param { string } content
+ * @param { boolean } withComments
  * @param { boolean } isASC
  */
 const Retrieve__Fuzzy = async (
   page: number,
   capacity: number,
   content: string,
+  withComments: boolean = true,
   isASC: boolean = false
 ): Promise<Restful> => {
   try {
@@ -88,6 +122,7 @@ const Retrieve__Fuzzy = async (
       (page - 1) * capacity,
       Number(capacity),
       content,
+      withComments,
       isASC
     );
     return new Restful(
