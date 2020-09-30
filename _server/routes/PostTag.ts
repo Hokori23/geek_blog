5821e4d52e351923e10c599f6f7c7a58e9dcd8ed
Hokori23 <hokori23@qq.com>
2020-10-01 01:18:02 +0800

    feat: PostTag Listen层

---
+++ _server/routes/PostTag.ts
@@ -0,0 +1,93 @@
+import { PostTagService as Service } from '@service';
+import { PostTag } from '@vo';
+import { Restful, isUndef } from '@public';
+const EXPRESS = require('express');
+const ROUTER = EXPRESS.Router();
+
+/**
+ * 通过标签名查找帖子（分页）
+ * @path /retrieve
+ */
+ROUTER.get('/retrieve', async (req, res, next) => {
+  const { name, page, capacity } = req.query;
+  try {
+    if (
+      !name &&
+      isUndef(page) &&
+      isUndef(capacity) &&
+      page < 0 &&
+      capacity < 0
+    ) {
+      res.status(200).json(new Restful(1, '参数错误'));
+    } else {
+      res
+        .status(200)
+        .json(await Service.Retrieve__ByTagName(name, page, capacity));
+    }
+  } catch (e) {
+    // 进行邮件提醒
+    res.status(500).end();
+  }
+  next();
+});
+
+/**
+ * 新建帖子标签
+ * @path /create
+ */
+ROUTER.post('/create', async (req, res, next) => {
+  const postTag: PostTag = PostTag.build(req.body);
+
+  try {
+    if (!PostTag.checkIntegrity(['name', 'slug'])) {
+      res.status(200).json(new Restful(1, '参数错误'));
+    } else {
+      res.status(200).json(await Service.Create(postTag, res.locals.userPower));
+    }
+  } catch (e) {
+    // 进行邮件提醒
+    res.status(500).end();
+  }
+  next();
+});
+
+/**
+ * 编辑
+ * @path /edit
+ */
+ROUTER.post('/edit', async (req, res, next) => {
+  const postTag: PostTag = PostTag.build(req.body);
+
+  try {
+    if (!PostTag.checkIntegrity(['id', 'name', 'slug'])) {
+      res.status(200).json(new Restful(1, '参数错误'));
+    } else {
+      res.status(200).json(await Service.Edit(postTag, res.locals.userPower));
+    }
+  } catch (e) {
+    // 进行邮件提醒
+    res.status(500).end();
+  }
+  next();
+});
+
+/**
+ * 删除
+ * @path /delete
+ */
+ROUTER.post('/delete', async (req, res, next) => {
+  const { id } = req.body;
+  try {
+    if (isUndef(id)) {
+      res.status(200).json(new Restful(1, '参数错误'));
+    } else {
+      res.status(200).json(await Service.Delete(id, res.locals.userPower));
+    }
+  } catch (e) {
+    // 进行邮件提醒
+    res.status(500).end();
+  }
+  next();
+});
+
+export default ROUTER;
