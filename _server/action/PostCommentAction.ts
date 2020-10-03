e7400107fe2f2eb97f524c2620fc59fc13d7b6cf
Hokori23 <hokori23@qq.com>
2020-10-04 00:25:43 +0800

    feat: 改为Sequelize关联查询 && 添加父子评论处理函数

--- _server/action/PostCommentAction.ts
+++ _server/action/PostCommentAction.ts
@@ -1,14 +1,6 @@
 import { PostComment } from '@vo';
 
 /**
- * 添加帖子评论
- * @param { PostComment } postComment
- */
-const Create = (postComment: PostComment): Promise<PostComment> => {
-  return postComment.save();
-};
-
-/**
  * 通过评论ID查询评论
  * @param { number } id
  */
@@ -16,18 +8,6 @@ const Retrieve__ByID = (id: number): Promise<PostComment | null> => {
   return PostComment.findOne({
     where: {
       id
-    }
-  });
-};
-
-/**
- * 通过帖子ID查询所有评论
- * @param { number } post_id
- */
-const Retrieve__ByPostID = (post_id: number): Promise<Array<PostComment>> => {
-  return PostComment.findAll({
-    where: {
-      post_id
     }
   });
 };
@@ -44,8 +24,6 @@ const Delete = (id: number): Promise<number> => {
   });
 };
 export default {
-  Create,
   Retrieve__ByID,
-  Retrieve__ByPostID,
   Delete
 };
--- _server/service/PostCommentService.ts
+++ _server/service/PostCommentService.ts
@@ -1,18 +1,52 @@
-import { PostCommentAction } from '@action';
+import { PostCommentAction as Action, PostAction } from '@action';
 import { PostComment } from '@vo';
 import { Restful, isUndef } from '@public';
 
 /**
  * 添加帖子评论
- * @param { PostComment } postComment
+ * @param { Object } postComment
  */
-const Create = async (postComment: PostComment): Promise<Restful> => {
+const Create = async (postComment): Promise<Restful> => {
   try {
-    postComment = await PostCommentAction.Create(postComment);
+    const post = await PostAction.Retrieve__ByID(postComment.post_id);
+    if (!post) {
+      return new Restful(1, '帖子不存在');
+    }
+    if (
+      postComment.parent_id &&
+      !post.PostComments.some(
+        (existedPostComment) => existedPostComment.id === postComment.parent_id
+      )
+    ) {
+      return new Restful(2, '回复评论不存在');
+    }
+    postComment = await post.createPostComment(postComment);
     return new Restful(0, '创建成功', postComment.toJSON());
   } catch (e) {
     return new Restful(99, `创建失败, ${e.message}`);
   }
+};
+
+/**
+ * 处理父子评论
+ */
+const FormatPostComments = (postComments: Array<PostComment>): Array<any> => {
+  const res: Array<any> = [];
+  const childComments = postComments.filter((postComment) => {
+    if (!postComment.parent_id) {
+      res.push({ ...postComment.toJSON(), childComments: [] });
+      return false;
+    }
+    return true;
+  });
+  childComments.forEach((childComment) => {
+    res.forEach((parentComments) => {
+      if (childComment.parent_id === parentComments.id) {
+        parentComments.childComments.push(childComment);
+      }
+    });
+  });
+  return res;
 };
 
 /**
@@ -21,13 +55,18 @@ const Create = async (postComment: PostComment): Promise<Restful> => {
  */
 const Retrieve__ByPostID = async (post_id: number): Promise<Restful> => {
   try {
-    const postComments = await PostCommentAction.Retrieve__ByPostID(post_id);
+    const post = await PostAction.Retrieve__ByID(post_id);
+    if (!post) {
+      return new Restful(1, '帖子不存在');
+    }
     return new Restful(
       0,
       '查询成功',
-      postComments.map((postComment) => {
-        return postComment.toJSON();
-      })
+      FormatPostComments(
+        await post.getPostComments({
+          order: [['createdAt', 'ASC']]
+        })
+      )
     );
   } catch (e) {
     return new Restful(99, `查询失败, ${e.message}`);
@@ -44,11 +83,11 @@ const Delete = async (id: number, userPower: number) => {
     if (isUndef(userPower) || userPower > 1) {
       return new Restful(2, `删除失败, 权限不足`);
     }
-    const existedComment = await PostCommentAction.Retrieve__ByID(id);
+    const existedComment = await Action.Retrieve__ByID(id);
     if (!existedComment) {
       return new Restful(1, '评论不存在');
     }
-    const deleteRow = await PostCommentAction.Delete(id);
+    const deleteRow = await Action.Delete(id);
     return deleteRow > 0
       ? new Restful(0, '删除成功')
       : new Restful(3, '删除失败');
