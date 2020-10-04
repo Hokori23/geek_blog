d4e48555b697ba9f2dbffc1c044cbe6c69e283fa
Hokori23 <hokori23@qq.com>
2020-10-05 00:00:36 +0800

    fix: 修正Sequelize建表前就监听端口的问题

--- _server/database/index.ts
+++ _server/database/index.ts
@@ -7,15 +7,19 @@ const DB = new Sequelize(database, user, password, {
   logging: process.env.NODE_ENV === 'development' ? console.log : false // 是否输出数据库日志
 });
 
-(async () => {
-  try {
-    // await DB.authenticate();
-    // console.log('Connection has been established successfully.');
-    await DB.sync({ alter: true });
-    console.log('All models were synchronized successfully.');
-  } catch (error) {
-    throw new Error(`Unable to connect to the database: ${error}`);
-  }
-})();
+const init = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // await DB.authenticate();
+      // console.log('Connection has been established successfully.');
+      await DB.sync({ alter: true });
+      console.log('All models were synchronized successfully.');
+      resolve();
+    } catch (error) {
+      throw new Error(`Unable to connect to the database: ${error}`);
+    }
+  });
+};
+export { init };
 
 export default DB;
