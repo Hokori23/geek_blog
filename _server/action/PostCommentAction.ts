ac1f350b476b3bbaf51fdbf7098da9569ca23889
Hokori23 <hokori23@qq.com>
2020-10-05 17:31:09 +0800

    feat: PostComment Action层 通过评论ID查询评论的子评论

--- _server/action/PostCommentAction.ts
+++ _server/action/PostCommentAction.ts
@@ -19,6 +19,20 @@ const Retrieve__ByID = (
 };
 
 /**
+ * 通过评论ID查询评论的子评论
+ * @param { number } parent_id
+ */
+const Retrieve__ChildrenComments__ByID = (
+  parent_id: number
+): Promise<Array<PostComment>> => {
+  return PostComment.findAll({
+    where: {
+      parent_id
+    }
+  });
+};
+
+/**
  * 通过评论ID删除评论
  * @param { number } id
  * @param { Transaction } ?t
@@ -34,5 +48,6 @@ const Delete = (id: number, t?: Transaction): Promise<number> => {
 
 export default {
   Retrieve__ByID,
+  Retrieve__ChildrenComments__ByID,
   Delete
 };
